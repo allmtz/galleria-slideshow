d2203993d78e378e53ff382d52f4849997a853b2
Allan <martinezallan123@gmail.com>
2022-12-17 15:59:51 -0800

    styling mobile info page

--- src/pages/PaintingInfo.jsx
+++ src/pages/PaintingInfo.jsx
@@ -11,30 +11,33 @@ export const PaintingInfo = ( {setFocusedPainting,focusedPainting, gallery} ) =>
 
 
     return(
-        <div>
-            <h1>{focusedPainting.name}</h1>
+        <div className="painting-page">
             <div className="img-container">
                 <img src={ "src/" + focusedPainting.images.hero.small} alt={focusedPainting.name} />
                 <div className="img-info">
-                    <h1>{focusedPainting.name}</h1>
+                    <h2>{focusedPainting.name}</h2>
                     <p>{focusedPainting.artist.name}</p>
                 </div>
-                <section className="description">
-                    <img src={"src/" + focusedPainting.artist.image} alt={focusedPainting.artist.name} className="artist-portrait" />
-                    <p>{focusedPainting.date}</p>
-                    <p>{focusedPainting.description}</p>
-                </section>
             </div>
 
+                <section className="painting-information">
+                    <img 
+                        src={"src/" + focusedPainting.artist.image} 
+                        alt={focusedPainting.artist.name} 
+                        className="artist-portrait" />
+                    <p className="year">{focusedPainting.year}</p>
+                    <p className="description">{focusedPainting.description}</p>
+                </section>
+
             <a href={focusedPainting.source}>go to source</a>
-            <progress value="1" max="100"></progress>
+            <progress value="1" max="10"></progress>
 
             <section className="slideshow-controls">
                 <div>
-                    <h3>{focusedPainting.name}</h3>
+                    <h4>{focusedPainting.name}</h4>
                     <p>{focusedPainting.artist.name}</p>
                 </div>
-                <div>
+                <div className="arrows" >
                     <img src={"src/assets/shared/icon-back-button.svg"} alt="back arrow" />
                     <img src={"src/assets/shared/icon-next-button.svg"} alt="forward arrow" />
                 </div>
