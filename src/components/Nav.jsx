addb042eea2d444c8c4fe5846b16a1b9acfd07fb
Allan <martinezallan123@gmail.com>
2022-12-23 12:49:46 -0800

    start/stop button works

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,31 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { gallery } from "../data";
 
+let isPlaying = false
+
 export const Nav = ( {focusedPainting}  ) => {
   const location = useLocation()
   const navigate = useNavigate()
 
   function startSlideshow(location){
-    //chooses where to start the slideshow depending on the current route
+    isPlaying = !isPlaying
+
+    //chooses at which painting to start the slideshow based on the current route
     let i = location.pathname === "/" ? 0 : gallery.indexOf(focusedPainting)
     
     navigate(`/${gallery[i].name}`)
 
-      let intervalID = setInterval( ()=>{
-        i++
-        if(i< gallery.length){
-          navigate(`/${gallery[i].name}`)
-          return
-        }
-        console.log("cleared")
-        clearInterval(intervalID)
-      },500 )
-  
+    let intervalID = setInterval( ()=>{
+      i++
+      if(i< gallery.length && isPlaying){
+        navigate(`/${gallery[i].name}`)
+        return
+      }
+      console.log("cleared")
+      clearInterval(intervalID)
+      isPlaying = false
+      i === gallery.length ? navigate(`/${gallery[gallery.length - 1].name}`) : ""
+    },500 )
   }
 
   return (
@@ -28,7 +33,7 @@ export const Nav = ( {focusedPainting}  ) => {
       <Link to="/">
         <h1>galleria.</h1>
       </Link>
-      <p onClick={ () => startSlideshow(location)}>start slideshow</p>
+      <p onClick={ () => startSlideshow(location)}>{!isPlaying ? "start" : "stop"} slideshow</p>
     </nav>
   );
 };
