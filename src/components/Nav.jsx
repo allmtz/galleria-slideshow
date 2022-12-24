62836d8b38a002e2fcd31a0c6d7abdfbec5d7cc6
Allan Martinez <martinezallan123@gmail.com>
2022-12-23 18:54:18 -0800

    refactor slideshow to use more React

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,39 +1,49 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { gallery } from "../data";
+import { useRef, useState } from "react";
 
-let isPlaying = false
 
 export const Nav = ( {focusedPainting}  ) => {
   const location = useLocation()
   const navigate = useNavigate()
 
-  function startSlideshow(location){
-    isPlaying = !isPlaying
+  const intervalContainer = useRef(null)
+  const [play,setPlay] =  useState(false)
 
-    //chooses at which painting to start the slideshow based on the current route
-    let i = location.pathname === "/" ? 0 : gallery.indexOf(focusedPainting)
-    
-    navigate(`/${gallery[i].name}`)
-
-    let intervalID = setInterval( ()=>{
-      i++
-      if(i< gallery.length && isPlaying){
-        navigate(`/${gallery[i].name}`)
-        return
-      }
-      console.log("cleared")
-      clearInterval(intervalID)
-      isPlaying = false
-      i === gallery.length ? navigate(`/${gallery[gallery.length - 1].name}`) : ""
-    },500 )
+  function stopSlideshow(){
+    setPlay(false)
+    clearInterval(intervalContainer.current)
   }
 
+  function startSlideshow(location){
+    if(play === false){
+      setPlay(true)
+
+      //chooses at which painting to start the slideshow based on the current route
+      let i = location.pathname === "/" ? 0 : gallery.indexOf(focusedPainting)
+      
+      // prevent navigating to the same painting twice if starting the slideshow from a specific painting
+      i === 0 ? navigate( `/${gallery[i].name}` ) : ""
+  
+      intervalContainer.current = setInterval( ()=>{
+        i++
+        if(i < gallery.length){
+          navigate( `/${gallery[i].name}` )
+          return
+        }
+        stopSlideshow()
+      },500 )
+      return
+    }
+    // stopping the slideshow before it reaches the last painting
+    stopSlideshow()
+  }
   return (
     <nav>
       <Link to="/">
         <h1>galleria.</h1>
       </Link>
-      <p onClick={ () => startSlideshow(location)}>{!isPlaying ? "start" : "stop"} slideshow</p>
+      <p onClick={ () => startSlideshow(location)}>{play ? "stop" : "start"} slideshow</p>
     </nav>
   );
 };
